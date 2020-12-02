8faba623ed835fa9f57b2a6396df04e3963d6cbf
kqarlos <kqarlos@hotmail.com>
2020-12-01 20:15:40 -0800

    Adds inquirer

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What's the project title?"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(answers => {
+        console.log("answers", answers);
 
+    });
 }
 
 // function call to initialize program
