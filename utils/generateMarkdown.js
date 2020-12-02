fd02d6142561869a1c6a8d25153169b1cb6af9f4
kqarlos <kqarlos@hotmail.com>
2020-12-01 20:57:02 -0800

    Updates readme template with badges and license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,10 +4,19 @@ function generateMarkdown(data) {
   
   # ${data.title}
 
+  ![Languages](https://img.shields.io/github/languages/count/${data.authorGithub}/${data.repoName})
+  ![Top Language](https://img.shields.io/github/languages/top/${data.authorGithub}/${data.repoName})
+  ![Code Size](https://img.shields.io/github/languages/code-size/${data.authorGithub}/${data.repoName})
+  ![Repo Size](https://img.shields.io/github/repo-size/${data.authorGithub}/${data.repoName})
+  ![Total Lines](https://img.shields.io/tokei/lines/github/${data.authorGithub}/${data.repoName})
+  ![Inquirer V](https://img.shields.io/github/package-json/dependency-version/${data.authorGithub}/${data.repoName}/inquirer)
+  ![Last Commit](https://img.shields.io/github/last-commit/${data.authorGithub}/${data.repoName})
+  ![Followers](https://img.shields.io/github/followers/${data.authorGithub}?style=social)
+
+
   ## Description 
   
   ${data.description}
-  
   
   ## Table of Contents (Optional)
   
@@ -47,6 +56,8 @@ function generateMarkdown(data) {
 
   ## License
   
+  ![License](https://img.shields.io/github/license/${data.authorGithub}/${data.repoName})
+
 
 
 
